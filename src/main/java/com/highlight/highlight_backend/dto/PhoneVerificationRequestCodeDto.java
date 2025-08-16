666d62e6b65c4ea0de9ddf83b0b7fd12cc017e96
전우선 <jeonwooseon@naver.com>
2025-08-16 16:37:10 +0900

    fix: 휴대폰 인증 관련 누락된 DTO 클래스 추가

---
+++ src/main/java/com/highlight/highlight_backend/dto/PhoneVerificationRequestCodeDto.java
@@ -0,0 +1,24 @@
+package com.highlight.highlight_backend.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 휴대폰 인증 코드 요청 DTO
+ * 
+ * @author 전우선
+ * @since 2025.08.16
+ */
+@Getter
+@NoArgsConstructor
+public class PhoneVerificationRequestCodeDto {
+    
+    /**
+     * 휴대폰 번호
+     */
+    @NotBlank(message = "휴대폰 번호는 필수입니다")
+    @Pattern(regexp = "^010-\\d{4}-\\d{4}$", message = "휴대폰 번호 형식이 올바르지 않습니다 (010-0000-0000)")
+    private String phoneNumber;
+}
