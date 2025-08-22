56d11c667aaf3f1875279031dc9eadddfb2b181f
takch02 <lim70kr38@gmail.com>
2025-08-22 15:40:05 +0900

    feat : build 오류 해결

--- src/main/java/com/highlight/highlight_backend/dto/AuctionScheduleRequestDto.java
+++ src/main/java/com/highlight/highlight_backend/dto/AuctionScheduleRequestDto.java
@@ -8,6 +8,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
