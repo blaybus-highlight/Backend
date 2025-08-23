10178d82ac2ce8fdcf0e611159ce55733f61f5ca
takch02 <lim70kr38@gmail.com>
2025-08-23 17:25:46 +0900

    feat : import 문제 해결

--- src/main/java/com/highlight/highlight_backend/repository/ProductImageRepository.java
+++ src/main/java/com/highlight/highlight_backend/repository/ProductImageRepository.java
@@ -2,6 +2,8 @@ package com.highlight.highlight_backend.repository;
 
 import com.highlight.highlight_backend.domain.ProductImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
