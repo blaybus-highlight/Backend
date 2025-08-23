36f4092b6a1f97e0eb253d5931baf9fa0b7da61f
takch02 <lim70kr38@gmail.com>
2025-08-23 16:56:52 +0900

    feat : dto 에 getter 추가

--- src/main/java/com/highlight/highlight_backend/dto/MyPagePremiumImageResponseDto.java
+++ src/main/java/com/highlight/highlight_backend/dto/MyPagePremiumImageResponseDto.java
@@ -1,11 +1,13 @@
 package com.highlight.highlight_backend.dto;
 
 import com.highlight.highlight_backend.domain.ProductImage;
+import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
 
 @Setter
+@Getter
 public class MyPagePremiumImageResponseDto {
 
     private List<ProductImage> premiumImages;
