844d2b726d3d3745fe7f23a2fde58c176e18c711
takch02 <lim70kr38@gmail.com>
2025-08-24 14:38:52 +0900

    feat : RefreshTokenResponseDto Getter 추가

--- src/main/java/com/highlight/highlight_backend/dto/token/RefreshTokenResponseDto.java
+++ src/main/java/com/highlight/highlight_backend/dto/token/RefreshTokenResponseDto.java
@@ -1,8 +1,10 @@
 package com.highlight.highlight_backend.dto.token;
 
+import lombok.Getter;
 import lombok.Setter;
 
 @Setter
+@Getter
 public class RefreshTokenResponseDto {
 
     private String adminId;
